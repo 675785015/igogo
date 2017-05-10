7e569dab9bf805371f0fc191cc85cad6b4d6425a
xiazh <675785015@qq.com>
2017-05-10 22:55:10 +0800

    add  file server

---
+++ com.igogo.openwin/file/src/main/java/com/igogo/file/Application.java
@@ -0,0 +1,15 @@
+package com.igogo.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lenovo on 2017/5/7.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
